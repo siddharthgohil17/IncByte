73583b1b99f31db7df3a8586d45e7948e4eacc7c
SIDDHARTH <siddharthgohil07@gmail.com>
2023-08-04 11:23:12 +0530

    fix error when multiple commands add

--- src/component/inputcontrol.js
+++ src/component/inputcontrol.js
@@ -1,210 +1,203 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 
 const Chandrayaan3Control = () => {
-    const [position, setPosition] = useState({ x: 0, y: 0, z: 0 });
-    const [direction, setDirection] = useState('N');
-    const [commands, SetCommands] = useState("")
-    const [stopExecution, SetStopExecution] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0, z: 0 });
+  const [direction, setDirection] = useState('N');
+  const [initialPosition, setInitialPosition] = useState('0, 0, 0');
+  const [initialDirection, setInitialDirection] = useState('N');
+  const [commands, setCommands] = useState("");
+  const [stopExecution, setStopExecution] = useState(false); // Initially set to true
 
-    const moveforward = () => {
-        switch (direction) {
-            case 'N':
-                setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y + 1 }));
-                break;
-            case 'S':
-                setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y - 1 }));
-                break;
-            case 'E':
-                setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x + 1 }));
-                break;
-            case 'W':
-                setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x - 1 }));
-                break;
-            case 'U':
-                setPosition((prevPosition) => ({ ...prevPosition, z: prevPosition.z + 1 }));
-                break;
-            case 'P':
-                setPosition((prevPosition) => ({ ...prevPosition, z: prevPosition.z - 1 }));
-                break;
-            default:
-                break;
-        }
+  const moveForward = (currentPosition, currentDirection) => {
+    // Implement the logic to move the spacecraft one step forward based on its current direction
+    switch (currentDirection) {
+      case 'N':
+        return { x: currentPosition.x, y: currentPosition.y + 1, z: currentPosition.z };
+      case 'S':
+        return { x: currentPosition.x, y: currentPosition.y - 1, z: currentPosition.z };
+      case 'E':
+        return { x: currentPosition.x + 1, y: currentPosition.y, z: currentPosition.z };
+      case 'W':
+        return { x: currentPosition.x - 1, y: currentPosition.y, z: currentPosition.z };
+      case 'U':
+        return { x: currentPosition.x, y: currentPosition.y, z: currentPosition.z + 1 };
+      case 'D':
+        return { x: currentPosition.x, y: currentPosition.y, z: currentPosition.z - 1 };
+      default:
+        return currentPosition;
+    }
+  };
+
+  const moveBackward = (currentPosition, currentDirection) => {
+    switch (currentDirection) {
+      case 'N':
+        return { x: currentPosition.x, y: currentPosition.y - 1, z: currentPosition.z };
+      case 'S':
+        return { x: currentPosition.x, y: currentPosition.y + 1, z: currentPosition.z };
+      case 'E':
+        return { x: currentPosition.x - 1, y: currentPosition.y, z: currentPosition.z };
+      case 'W':
+        return { x: currentPosition.x + 1, y: currentPosition.y, z: currentPosition.z };
+      case 'U':
+        return { x: currentPosition.x, y: currentPosition.y, z: currentPosition.z - 1 };
+      case 'D':
+        return { x: currentPosition.x, y: currentPosition.y, z: currentPosition.z + 1 };
+      default:
+        return currentPosition;
+    }
+  };
+
+  const turnLeft = (currentDirection) => {
+    // Implement the logic to turn the spacecraft 90 degrees left based on its current direction
+    switch (currentDirection) {
+      case 'N':
+        return 'W';
+      case 'S':
+        return 'E';
+      case 'E':
+        return 'N';
+      case 'W':
+        return 'S';
+      case 'U':
+        return 'W';
+      case 'D':
+        return 'E';
+      default:
+        return currentDirection;
+    }
+  };
+
+  const turnRight = (currentDirection) => {
+    // Implement the logic to turn the spacecraft 90 degrees right based on its current direction
+    switch (currentDirection) {
+      case 'N':
+        return 'E';
+      case 'S':
+        return 'W';
+      case 'E':
+        return 'S';
+      case 'W':
+        return 'N';
+      case 'U':
+        return 'E';
+      case 'D':
+        return 'W';
+      default:
+        return currentDirection;
+    }
+  };
+
+  const turnUp = () => {
+    // Implement the logic to turn the spacecraft upward
+    return 'U';
+  };
+
+  const turnDown = () => {
+    // Implement the logic to turn the spacecraft downward
+    return 'D';
+  };
+
+  const handleCommandsSubmit = (e) => {
+    e.preventDefault();
+    setStopExecution(false); // Reset the stopExecution flag
+
+    if (commands.length === 0) {
+      return; // Don't proceed if there are no commands
     }
 
-    const movebackward = () => {
-        switch (direction) {
-            case 'N':
-                setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y - 1 }));
-                break;
-            case 'S':
-                setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y + 1 }));
-                break;
-            case 'E':
-                setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x - 1 }));
-                break;
-            case 'W':
-                setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x + 1 }));
-                break;
-            case 'U':
-                setPosition((prevPosition) => ({ ...prevPosition, z: prevPosition.z - 1 }));
-                break;
-            case 'P':
-                setPosition((prevPosition) => ({ ...prevPosition, z: prevPosition.z + 1 }));
-                break;
-            default:
-                break;
-        }
+    const commandArray = commands.split(','); // Split commands into an array
+
+    let currentPosition = { ...position };
+    let currentDirection = direction;
+
+    for (let command of commandArray) {
+      switch (command.trim().toLowerCase()) {
+        case "f":
+          currentPosition = moveForward(currentPosition, currentDirection);
+          break;
+        case "b":
+          currentPosition = moveBackward(currentPosition, currentDirection);
+          break;
+        case "r":
+          currentDirection = turnRight(currentDirection);
+          break;
+        case "l":
+          currentDirection = turnLeft(currentDirection);
+          break;
+        case "u":
+          currentDirection = turnUp();
+          break;
+        case "d":
+          currentDirection = turnDown();
+          break;
+        default:
+          break;
+      }
     }
+
+    // Update the final position and direction
+    setPosition(currentPosition);
+    setDirection(currentDirection);
+
+    // Once all commands are executed, set the stopExecution flag to true
+    setStopExecution(true);
+  };
+
+  const handleStopExecution = () => {
+    setStopExecution(true);
     
-    const turnLeft = () => {
-        // Implement the logic to turn the spacecraft 90 degrees left based on its current direction
-        switch (direction) {
-            case 'N':
-                setDirection('W');
-                break;
-            case 'S':
-                setDirection('E');
-                break;
-            case 'E':
-                setDirection('N');
-                break;
-            case 'W':
-                setDirection('S');
-                break;
-            case 'U':
-                setDirection('W');
-                break;
-            case 'D':
-                setDirection('W');
-                break;
+  };
 
-            default:
-                break;
-        }
-    };
+  const handleInitialValuesSubmit = (e) => {
+    e.preventDefault();
+    // Set the initial position and direction based on the user input
+    const [x, y, z] = initialPosition.split(',').map((val) => parseInt(val.trim(), 10));
+    setPosition({ x, y, z });
+    setDirection(initialDirection);
+  };
 
-    const turnRight = () => {
-        // Implement the logic to turn the spacecraft 90 degrees right based on its current direction
-        switch (direction) {
-            case 'N':
-                setDirection('E');
-                break;
-            case 'S':
-                setDirection('W');
-                break;
-            case 'E':
-                setDirection('S');
-                break;
-            case 'W':
-                setDirection('N');
-                break;
-            case 'U':
-                setDirection('E');
-                break;
-            case 'D':
-                setDirection('E');
-                break;
-            default:
-                break;
-        }
-    };
+  useEffect(() => {
+    console.log("Position updated:", position);
+  }, [position, direction]);
 
-    const turnUp = () => {
-        // Implement the logic to turn the spacecraft upward
-        setDirection('U');
-    };
+  return (
+    <div>
+      <h2>Chandrayaan 3 Lunar Craft Control</h2>
 
-    const turnDown = () => {
-        // Implement the logic to turn the spacecraft downward
-        setDirection('D');
-    };
+      <form onSubmit={handleInitialValuesSubmit}>
+        <label>
+          Enter Initial Position (x, y, z):
+          <input type="text" value={initialPosition} onChange={(e) => setInitialPosition(e.target.value)} required />
+        </label>
+        <label>
+          Enter Initial Direction:
+          <input type="text" value={initialDirection} onChange={(e) => setInitialDirection(e.target.value)} required />
+        </label>
+        <button type="submit">Set Initial Values</button>
+      </form>
 
+      <p>Current Position: ({position.x}, {position.y}, {position.z})</p>
+      <p>Current Direction: {direction}</p>
 
-    const handleCommands = (e) => {
-        e.preventDefault();
-        SetStopExecution(false);
-        const commandsArray = commands.split(",");
-        for (let command of commandsArray) {
-            switch (command) {
-                case 'f':
-                    moveforward();
-                    break;
-                case 'b':
-                    movebackward();
-                    break;
-                case 'r':
-                    turnRight();
-                    break;
-                case 'l':
-                    turnLeft();
-                    break;
-                case 'u':
-                    turnUp();
-                    break;
-                case 'd':
-                    turnDown();
-                    break;
-                default:
-                    break;
-            }
-         
-        }
-        SetStopExecution(true);
-    }
+      <form onSubmit={handleCommandsSubmit}>
+        <label>
+          Enter Commands:
+          <input type="text" value={commands} onChange={(e) => setCommands(e.target.value)} required />
+        </label>
+        <button type="submit" onClick={handleStopExecution}>Execute Commands</button>
+       
+      </form>
 
-    const handleDeclareResutlt = (e) => {
-        e.preventDefault();
-        SetCommands([]);
-        
-        SetStopExecution(true);
-    }
-
-
-    const handleInitialValuesSubmit = (e) => {
-        e.preventDefault();
-        const [x, y, z] = e.target.position.value.split(',').map((val) => parseInt(val.trim(), 10));
-        setPosition({ x, y, z });
-        setDirection(e.target.direction.value);
-    };
-
-    return (
+      {/* Display the final position and direction here */}
+      {stopExecution && (
         <div>
-            <h2>Chandrayaan 3 Lunar Craft Control</h2>
-            <form onSubmit={handleInitialValuesSubmit}>
-                <label>
-                    Enter Initial Position (x, y, z):
-                    <input type="text" name="position" defaultValue={`${position.x}, ${position.y}, ${position.z}`} />
-                </label>
-                <label>
-                    Enter Initial Direction:
-                    <input type="text" name="direction" defaultValue={direction} />
-                </label>
-                <button type="submit">Set Initial Values</button>
-            </form>
-            <p>Current Position: ({position.x}, {position.y}, {position.z})</p>
-            <p>Current Direction: {direction}</p>
-
-            <form onSubmit={handleCommands}>
-                <label>
-                    Enter Commands:
-                    <input type="text" value={commands} onChange={(e) => SetCommands(e.target.value)} />
-                    <button type="submit">Execute Commands</button>
-                    <button onClick={handleDeclareResutlt}>Stop</button>
-                </label>
-            </form>
-
-            {stopExecution && (
-                <div>
-                    <h3>Final Position:</h3>
-                    <p>Position: ({position.x}, {position.y}, {position.z})</p>
-                    <p>Direction: {direction}</p>
-                </div>
-            )}
-
-
-
+          <h3>Final Position:</h3>
+          <p>Position: ({position.x}, {position.y}, {position.z})</p>
+          <p>Direction: {direction}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Chandrayaan3Control;
